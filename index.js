551ac2d87c66e74494921fde1c99b0dc75cf4057
Mathias Munene <munenemathias@gmail.com>
2025-04-13 08:24:27 +0300

    first commmit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function razzle (lawyer = "Billy", target = "`em"){
+    console.log(`${lawyer} razzle-dazzle ${target}!`);
+}
+razzle();
+razzle("Methuselah", "T'challah");
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+const mondayWork = function(activity = "go to the office"){
+    return `this Monday, I will ${activity}`;
+}
