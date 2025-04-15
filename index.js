4d31f41d45e099ce36067394d07f0032f51968d8
Mathias Munene <munenemathias@gmail.com>
2025-04-15 23:17:27 +0300

    final commit

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`;
   }
 
-const mondayWork = function(activity = "go to the office"){
-    return `this Monday, I will ${activity}`;
-}
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`; 
+  };
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
